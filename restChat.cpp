3caf71c820878971760407582dd57f95d8ff8eed
JigJackfriut <124086458+JigJackfriut@users.noreply.github.com>
2023-05-07 11:32:45 -0400

    Update restChat.cpp

--- restChat.cpp
+++ restChat.cpp
@@ -24,16 +24,91 @@ const int port = 5005;
 
 
 
+void addMessage(string username, string message, map<string,vector<string>> &messageMap) {
+	/* iterate through users adding message to each */
+	string jsonMessage = "{\"user\":\""+username+"\",\"message\":\""+message+"\"}";
+	for (auto userMessagePair : messageMap) {
+		username = userMessagePair.first;
+		messageMap[username].push_back(jsonMessage);
+	}
+}
+
+string getMessagesJSON(string username, map<string,vector<string>> &messageMap) {
+	/* retrieve json list of messages for this user */
+	bool first = true;
+	string result = "{\"messages\":[";
+	for (string message :  messageMap[username]) {
+		if (not first) result += ",";
+		result += message;
+		first = false;
+	}
+	result += "]}";
+	messageMap[username].clear();
+	return result;
+}
+
+
+
+
+
+
+
 
 int main(void) {
   Server svr;
   restDB rDB;
+  map<string,vector<string>> messageMap;
 
 	
   /* "/" just returnsAPI name */
   svr.Get("/", [](const Request & /*req*/, Response &res) {
     res.set_header("Access-Control-Allow-Origin","*");
     res.set_content("Chat API", "text/plain");
+  });
+	
+	
+	
+	
+	
+	
+	svr.Get(R"(/chat/join/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+    string username = req.matches[1];
+    string result;
+    vector<string> empty;
+    cout << username << " joins" << endl;
+    
+    // Check if user with this name exists
+    if (messageMap.count(username)) {
+    	result = "{\"status\":\"exists\"}";
+    } else {
+    	// Add user to messages map
+    	messageMap[username]=empty;
+    	result = "{\"status\":\"success\",\"user\":\"" + username + "\"}";
+    }
+    res.set_content(result, "text/json");
+  });
+
+   svr.Get(R"(/chat/send/(.*)/(.*))", [&](const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin","*");
+	string username = req.matches[1];
+	string message = req.matches[2];
+	string result; 
+	
+    if (!messageMap.count(username)) {
+    	result = "{\"status\":\"baduser\"}";
+	} else {
+		addMessage(username,message,messageMap);
+		result = "{\"status\":\"success\"}";
+	}
+    res.set_content(result, "text/json");
+  });
+  
+   svr.Get(R"(/chat/fetch/(.*))", [&](const Request& req, Response& res) {
+    string username = req.matches[1];
+    res.set_header("Access-Control-Allow-Origin","*");
+    string resultJSON = getMessagesJSON(username,messageMap);
+    res.set_content(resultJSON, "text/json");
   });
 
 	
